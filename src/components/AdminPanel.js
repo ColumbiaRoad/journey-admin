db53398700f70da632ea7bd526d731f347b81217
Max Reuter <max.a.reuter@gmail.com>
2017-08-14 16:10:09 +0300

    add root question to admin panel and update empty state

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Page, Layout, Banner } from '@shopify/polaris';
+import { Page, Layout, EmptyState } from '@shopify/polaris';
 
 import SelectedProductListContainer from '../containers/SelectedProductListContainer'
 import ProductPickerContainer from '../containers/ProductPickerContainer';
+import RootQuestionContainer from '../containers/RootQuestionContainer';
 
 const AdminPanel = (props) => {
   return (
@@ -10,21 +11,26 @@ const AdminPanel = (props) => {
       <Layout>
         <Layout.Section>
         { !props.selection &&
-            <Banner
-              title="No products selected"
+            <EmptyState
+              heading="No products selected"
               action={{
                 content: 'Select products',
                 onAction: () => { props.onToggle('set') }
               }}
+              image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
             >
               <p>Select products to create questions and map possible answers to product option values.</p>
-            </Banner> }
+            </EmptyState>
+        }
         { (process.env.NODE_ENV !== 'development') &&
             <ProductPickerContainer /> }
         </Layout.Section>
         <Layout.Section>
           { props.selection &&
-              <SelectedProductListContainer /> }
+            <div>
+              <RootQuestionContainer />
+              <SelectedProductListContainer />
+            </div> }
         </Layout.Section>
       </Layout>
     </Page>
