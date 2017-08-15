8568586cd727fcf3145626ef30192e375d0d7073
Max Reuter <max.a.reuter@gmail.com>
2017-08-15 13:25:23 +0300

    persist store after refresh

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AdminPanelContainer from './containers/AdminPanelContainer';
 import reducer from './reducers/adminPanel';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import '@shopify/polaris/styles.css';
 import { EmbeddedApp } from '@shopify/polaris/embedded';
 import dotenv from 'dotenv';
 import URLSearchParams from 'url-search-params';
 import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { autoRehydrate, persistStore } from 'redux-persist';
 
 import { setJwtToken } from './actions/jwtToken';
 import { setSelectedProducts } from './actions/selectedProducts';
@@ -20,17 +22,14 @@ const shopOrigin = (shop) ? `https://${shop}` : undefined;
 const token = new URLSearchParams(window.location.search).get('token');
 const apiKey = process.env.REACT_APP_SHOPIFY_API_KEY;
 
-const store = createStore(
-  reducer,
-  applyMiddleware(ReduxThunk)
-);
+const store = compose(
+  applyMiddleware(ReduxThunk, logger),
+  autoRehydrate()
+)(createStore)(reducer);
+
+persistStore(store);
 
 store.dispatch(setJwtToken(token));
-
-// Log every state change
-store.subscribe(() =>
-  console.log(store.getState())
-)
 
 if (process.env.NODE_ENV !== 'development') {
   ReactDOM.render(
