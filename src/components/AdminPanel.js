3597bd0d23249f83dd93b8c9fb4cff9168fcbea7
Max Reuter <max.a.reuter@gmail.com>
2017-09-05 14:22:40 +0300

    refactor admin panel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -14,6 +14,11 @@ class AdminPanel extends React.Component {
     this.onSave = this.onSave.bind(this);
   }
 
+  /**
+   * Check validity of all parsing reports
+   * @param {Object} allParsingReports
+   * @returns {boolean} errorsPresent
+   */
   containsErrors(allParsingReports) {
     const { rootQuestion, ...selectedProducts } = allParsingReports;
     const validity = [
@@ -24,10 +29,16 @@ class AdminPanel extends React.Component {
         })
       })
     ];
+    // Flatmap arrays and check whether all are valid
     return [].concat.apply([], validity).includes(false);
   }
 
-  buildQuestionnaire() {
+  /**
+   * Remove product information from questionnaire before saving
+   * it to database.
+   * @returns {Object}
+   */
+  reduceQuestionnaire() {
     const questionnaire = {};
     questionnaire.rootQuestion = {
       question: this.props.rootQuestion.question,
@@ -42,7 +53,66 @@ class AdminPanel extends React.Component {
     return questionnaire;
   }
 
+  /**
+   * Restores reduced questionnaire from database to the original version
+   * that includes product information.
+   * @param {Object} questionnaire 
+   * @param {Array<Object>} products
+   * @returns {Object} originalQuestionnaire
+   */
+  restoreQuestionnaire(questionnaire, products) {
+    return {
+      rootQuestion: {
+        ...questionnaire.rootQuestion,
+        answerMapping: questionnaire.rootQuestion.answerMapping.map((mapping) => {
+          return {
+            ...mapping,
+            value: parseInt(mapping.value, 10)
+          };
+        })
+      },
+      selectedProducts: questionnaire.selectedProducts.map((item) => {
+        return {
+          product: products.find(e => e.id === parseInt(item.productId, 10)),
+          questions: item.questions
+        };
+      })
+    };
+  }
+
+  /**
+   * Fetch products with passed IDs from Shopify through backend API.
+   * Included fields are:
+   * - id
+   * - opitons
+   * - title
+   * @param {string} productIds
+   * @return {Promise<Array>} Products
+   */
+  fetchProducts(productIds) {
+    return new Promise((resolve, reject) => {
+      fetch(`/api/v1/products?ids=${productIds}&fields=id,options,title`, {
+        method: 'get',
+        headers: { 'Authorization': `Bearer ${this.props.token}` }
+      })
+      .then((response) => {
+        if(response.status === 200) {
+          return response.json();
+        } else {
+          Promise.reject(response.text());
+        }
+      })
+      .then((json) => {
+        resolve(json);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
   onSave() {
+    // Get parsing report of all selected products and update redux store
     const allParsingReports = parseProductSelectionAnswerMappings(this.props.selectedProducts);
     const rootAllowedValues = this.props.selectedProducts.map(item => item.product.id);
     allParsingReports.rootQuestion = parseAnswerMapping({
@@ -50,6 +120,8 @@ class AdminPanel extends React.Component {
       answerMapping: this.props.rootQuestion.answerMapping
     }, rootAllowedValues);
     this.props.onSave(allParsingReports);
+
+    // Save questionnaire to database
     if(!this.containsErrors(allParsingReports)) {
       fetch(`/api/v1/shop/questionnaire`, {
         method: 'post',
@@ -58,7 +130,7 @@ class AdminPanel extends React.Component {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          questionnaire: this.buildQuestionnaire()
+          questionnaire: this.reduceQuestionnaire() // Remove unnecessary info before saving questionnaire
         })
       })
       .then((response) => {
@@ -74,48 +146,31 @@ class AdminPanel extends React.Component {
   }
 
   componentDidMount() {
+    // Try to fetch questionnaire from database
     let questionnaire;
     fetch(`/api/v1/shop/questionnaire`, {
       method: 'get',
       headers: { 'Authorization': `Bearer ${this.props.token}` },
     })
     .then((response) => {
-      return response.json();
+      if(response.status === 200) {
+        return response.json();
+      } else {
+        Promise.reject(response.text());
+      }
     })
     .then((json) => {
       if (json.status === 'ok') {
         questionnaire = json.questionnaire;
         const productIds = questionnaire.selectedProducts.map(item => item.productId).join();
-        return fetch(`/api/v1/products?ids=${productIds}&fields=id,options,title`, {
-          method: 'get',
-          headers: { 'Authorization': `Bearer ${this.props.token}` }
-        })
+        return this.fetchProducts(productIds);
       } else {
         return Promise.reject('No existing questionnaire found');
       }
     })
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      const storeQuestionnaire = {
-        rootQuestion: {
-          ...questionnaire.rootQuestion,
-          answerMapping: questionnaire.rootQuestion.answerMapping.map((mapping) => {
-            return {
-              ...mapping,
-              value: parseInt(mapping.value, 10)
-            };
-          })
-        },
-        selectedProducts: questionnaire.selectedProducts.map((item) => {
-          return {
-            product: json.find(e => e.id === parseInt(item.productId, 10)),
-            questions: item.questions
-          };
-        })
-      };
-      this.props.onFetch(storeQuestionnaire);
+    .then((products) => {
+      // Save restored questionnaire in store
+      this.props.onQuestionnaire(this.restoreQuestionnaire(questionnaire, products));
     })
     .catch((err) => {
       console.error(err);
