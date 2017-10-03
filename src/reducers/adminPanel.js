bd76aa0e98816a88a605f36435287a142a6bf4ef
Max Reuter <max.a.reuter@gmail.com>
2017-10-03 16:29:07 +0300

    update root reducer

--- src/reducers/adminPanel.js
+++ src/reducers/adminPanel.js
@@ -2,13 +2,15 @@ import productPicker from './productPicker';
 import jwtToken from './jwtToken';
 import selectedProducts from './selectedProducts';
 import rootQuestion from './rootQuestion';
+import dataSource from './dataSource';
 import { combineReducers } from 'redux';
 
 const adminPanel = combineReducers({
   productPicker,
   jwtToken,
   selectedProducts,
-  rootQuestion
+  rootQuestion,
+  dataSource
 });
 
 export default adminPanel;
